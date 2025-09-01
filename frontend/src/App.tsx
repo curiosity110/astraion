a4140750cdda6159d24ce40f730ffe6164722ffc
Georgie Delev <georgiedelev@gmail.com>
2025-09-01 17:53:59 +0200

    docker everything persistent WebSocket works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import ClientsList from './pages/ClientsList';
-import ClientProfile from './pages/ClientProfile';
-import TripsList from './pages/TripsList';
-import TripDetail from './pages/TripDetail';
-import Dashboard from './pages/Dashboard';
-import ActivityFeed from './pages/ActivityFeed';
-import { WebSocketProvider } from './components/WebSocketProvider';
+import ClientsList from "./pages/ClientsList";
+import ClientProfile from "./pages/ClientProfile";
+import TripsList from "./pages/TripsList";
+import TripDetail from "./pages/TripDetail";
+import Dashboard from "./pages/Dashboard";
+import ActivityFeed from "./pages/ActivityFeed";
+import { WebSocketProvider } from "./components/WebSocketProvider";
 
 function App() {
   const path = window.location.pathname;
   let page;
-  if (path.startsWith('/clients/')) {
-    const id = path.split('/')[2];
+  if (path.startsWith("/clients/")) {
+    const id = path.split("/")[2];
     page = <ClientProfile id={id} />;
-  } else if (path === '/clients') {
+  } else if (path === "/clients") {
     page = <ClientsList />;
-  } else if (path.startsWith('/trips/')) {
-    const id = path.split('/')[2];
+  } else if (path.startsWith("/trips/")) {
+    const id = path.split("/")[2];
     page = <TripDetail id={id} />;
-  } else if (path === '/trips') {
+  } else if (path === "/trips") {
     page = <TripsList />;
-  } else if (path === '/activity') {
+  } else if (path === "/activity") {
     page = <ActivityFeed />;
   } else {
     page = <Dashboard />;
